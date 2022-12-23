cda6a7e1f87ccad1d306230f5f87f1886eb6ebd4
Homero José <96498455+rox651@users.noreply.github.com>
2022-12-23 10:16:32 -0500

    Add files via upload

--- index.js
+++ index.js
@@ -17,90 +17,98 @@ let counterHours = 0;
 let counterDays = 0;
 
 //functions and eventsListener
+
+function formatNumbers(number) {
+   return number.toString().padStart(2, "0");
+}
+
 window.addEventListener("load", () => {
-  //get and put the vars on the dom when the page loads
-  counterSeconds = localStorage.getItem("seconds");
-  counterMinutes = localStorage.getItem("minutes");
-  counterHours = localStorage.getItem("hours");
-  counterDays = localStorage.getItem("days");
-  seconds.textContent = counterSeconds.toString().padStart(2, "0");
-  minutes.textContent = counterMinutes.toString().padStart(2, "0");
-  hours.textContent = counterHours.toString().padStart(2, "0");
-  days.textContent = counterDays.toString().padStart(2, "0");
+   //get and put the vars on the dom when the page loads
+   counterSeconds = localStorage.getItem("seconds") || 0;
+   counterMinutes = localStorage.getItem("minutes") || 0;
+   counterHours = localStorage.getItem("hours") || 0;
+   counterDays = localStorage.getItem("days") || 0;
+   seconds.textContent = formatNumbers(counterSeconds) || 0;
+   minutes.textContent = formatNumbers(counterMinutes) || 0;
+   hours.textContent = formatNumbers(counterHours) || 0;
+   days.textContent = formatNumbers(counterDays) || 0;
 });
+
 start.addEventListener("click", () => {
-  // add and remove atributes
-  start.setAttribute("disabled", "disabled");
-  stop.removeAttribute("disabled");
-  reset.removeAttribute("disabled");
-  numbersCtn.classList.add("colorTimer");
+   // add and remove atributes
+   start.setAttribute("disabled", "disabled");
+   stop.removeAttribute("disabled");
+   reset.removeAttribute("disabled");
+   numbersCtn.classList.add("colorTimer");
 
-  //setInterval of timer
-  const timerInterval = setInterval(() => {
-    //verify if the vars are equal to 60, if that is true, then reset to 0, and over start again
+   //setInterval of timer
+   const timerInterval = setInterval(() => {
+      //verify if the vars are equal to 60, if that is true, then reset to 0, and over start again
 
-    if (counterSeconds !== 60) {
-      counterSeconds++;
-      seconds.textContent = counterSeconds.toString().padStart(2, "0");
-    }
-    if (counterSeconds == 60) {
+      if (counterSeconds !== 60) {
+         counterSeconds++;
+         seconds.textContent = formatNumbers(counterSeconds);
+      }
+      if (counterSeconds == 60) {
+         counterSeconds = 0;
+         seconds.textContent = formatNumbers(counterSeconds);
+         counterMinutes++;
+         minutes.textContent = formatNumbers(counterMinutes);
+      }
+      if (counterMinutes == 60) {
+         counterMinutes = 0;
+         minutes.textContent = formatNumbers(counterMinutes);
+         counterHours++;
+         hours.textContent = formatNumbers(counterHours);
+      }
+      if (counterHours == 24) {
+         counterHours = 0;
+         hours.textContent = formatNumbers(counterHours);
+         counterDays++;
+         days.textContent = formatNumbers(counterDays);
+      }
+
+      //Set vars on the localStorage
+      localStorage.setItem("seconds", counterSeconds);
+      localStorage.setItem("minutes", counterMinutes);
+      localStorage.setItem("hours", counterHours);
+      localStorage.setItem("days", counterDays);
+
+      //show the same timer but on the page title
+      document.title = `
+      ${formatNumbers(counterDays)}:
+      ${formatNumbers(counterHours)}:
+      ${formatNumbers(counterMinutes)}:
+      ${formatNumbers(counterSeconds)}`;
+   }, 1000);
+
+   //stop and clean of inverval
+   stop.addEventListener("click", () => {
+      //add and remove attributes
+      clearInterval(timerInterval);
+      stop.setAttribute("disabled", "disabled");
+      start.removeAttribute("disabled");
+      numbersCtn.classList.remove("colorTimer");
+   });
+
+   //reset the vars and the variables
+   reset.addEventListener("click", () => {
+      clearInterval(timerInterval);
+      start.removeAttribute("disabled");
+      stop.setAttribute("disabled", "disabled");
+      numbersCtn.classList.remove("colorTimer");
       counterSeconds = 0;
-      seconds.textContent = counterSeconds.toString().padStart(2, "0");
-      counterMinutes++;
-      minutes.textContent = counterMinutes.toString().padStart(2, "0");
-    }
-    if (counterMinutes == 60) {
       counterMinutes = 0;
-      minutes.textContent = counterMinutes.toString().padStart(2, "0");
-      counterHours++;
-      hours.textContent = counterHours.toString().padStart(2, "0");
-    }
-    if (counterHours == 24) {
       counterHours = 0;
-      hours.textContent = counterHours.toString().padStart(2, "0");
-      counterDays++;
-      days.textContent = counterDays.toString().padStart(2, "0");
-    }
-
-    //Set vars on the localStorage
-    localStorage.setItem("seconds", counterSeconds);
-    localStorage.setItem("minutes", counterMinutes);
-    localStorage.setItem("hours", counterHours);
-    localStorage.setItem("days", counterDays);
-
-    //show the same timer but on the page title
-    document.title = `Timer:
-      ${counterDays.toString().padStart(2, "0")}:
-      ${counterHours.toString().padStart(2, "0")}:
-      ${counterMinutes.toString().padStart(2, "0")}:
-      ${counterSeconds.toString().padStart(2, "0")}`;
-  }, 1000);
-
-  //stop and clean of inverval
-  stop.addEventListener("click", () => {
-    //add and remove attributes
-    clearInterval(timerInterval);
-    stop.setAttribute("disabled", "disabled");
-    start.removeAttribute("disabled");
-  });
-
-  //reset the vars and the variables
-  reset.addEventListener("click", () => {
-    clearInterval(timerInterval);
-    start.removeAttribute("disabled");
-    stop.setAttribute("disabled", "disabled");
-    counterSeconds = 0;
-    counterMinutes = 0;
-    counterHours = 0;
-    counterDays = 0;
-    //reset and set vars on the local storage
-    localStorage.setItem("seconds", counterSeconds);
-    localStorage.setItem("minutes", counterMinutes);
-    localStorage.setItem("hours", counterHours);
-    localStorage.setItem("days", counterDays);
-    seconds.textContent = counterSeconds.toString().padStart(2, "0");
-    minutes.textContent = counterMinutes.toString().padStart(2, "0");
-    hours.textContent = counterHours.toString().padStart(2, "0");
-    days.textContent = counterDays.toString().padStart(2, "0");
-  });
+      counterDays = 0;
+      //reset and set vars on the local storage
+      localStorage.setItem("seconds", counterSeconds);
+      localStorage.setItem("minutes", counterMinutes);
+      localStorage.setItem("hours", counterHours);
+      localStorage.setItem("days", counterDays);
+      seconds.textContent = formatNumbers(counterSeconds);
+      minutes.textContent = formatNumbers(counterMinutes);
+      hours.textContent = formatNumbers(counterHours);
+      days.textContent = formatNumbers(counterDays);
+   });
 });
